5111f89bcd12075d209f3ae457a1ff716b9d336c
nazmulhasan7447 <nazmulhasan747406@gmail.com>
2023-05-04 21:05:50 +0600

    added authentication feature

---
+++ core/urls.py
@@ -0,0 +1,15 @@
+from django.contrib import admin
+from django.urls import path, include
+from django.urls import re_path
+from . import views
+
+
+urlpatterns = [
+    re_path(r'^index/$', views.index, name='index'),
+    re_path(r'^$', views.home, name='home'),
+    re_path(r'^register/$', views.registration, name='registration'),
+    re_path(r'^login/$', views.login_user, name='login'),
+    re_path(r'^logout/$', views.logoutUser, name='logout'),
+]
+
+
--- core/views.py
+++ core/views.py
@@ -1,3 +1,114 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect, get_object_or_404
+from django.contrib.auth import login, logout, authenticate
+from django.contrib import messages
+from django.contrib.auth.decorators import login_required
+from django.contrib.auth.models import User
 
-# Create your views here.
+@login_required(login_url='/login')
+def index(request):
+    return render(request, 'index.html')
+
+@login_required(login_url='/login')
+def home(request):
+
+    users = User.objects.all()
+
+    context = {
+        'users': users
+    }
+
+    return render(request, 'home.html', context)
+
+
+def registration(request):
+
+    if request.method == 'POST':
+        name = request.POST['name']
+        email = request.POST['email']
+        username = request.POST['username']
+        password = request.POST['password']
+        confirmpass = request.POST['con_pass']
+        type = request.POST['type']
+
+
+        if name and email and username and password and confirmpass:
+            if  len(User.objects.filter(email=email)) <= 0 and len(User.objects.filter(username=username)) <= 0:
+                if password == confirmpass:
+                    try:
+                        if type == 'admin':
+                            user_account = User.objects.create_superuser(email=email, username=username, password=password)
+                            user_account.first_name = name
+                            user_account.save()
+
+                            # # sending verification email with code*********************************
+
+                            # subject = f"Verification Link"
+                            # verification_url = f"Click to verify account: http://127.0.0.1:8000/user/account/veirfication/{username}/{phone}/"
+                            # html_content = render_to_string('backEnd_superAdmin/verification_template.html',
+                            #                                 context={'verification_url': verification_url})
+                            # email = EmailMessage(subject, html_content, to=[email])
+                            # email.content_subtype = 'html'
+                            # EmailThreading(email).start()
+                            # messages.success(request, "Verification link sent to your email!")
+                            messages.success(request, "Account has been created successfully!")
+                            return redirect('registration')
+                        else:
+                            user_account = User.objects.create_user(email=email, username=username,
+                                                                         password=password)
+                            user_account.first_name = name
+                            user_account.save()
+                            messages.success(request, "Account has been created successfully!")
+                            return redirect('registration')
+                    except:
+                        messages.warning(request, "Can't create account! Try again!")
+                        return redirect('registration')
+                    # ends sending verification email with code*************************
+
+                else:
+                    messages.warning(request, "Password didn't match! Try again!")
+                    return redirect('registration')
+            else:
+                messages.warning(request, "User already exists!")
+                return redirect('registration')
+
+    return render(request, 'registration.html')
+
+
+def login_user(request):
+
+    if request.method == 'POST':
+        username = request.POST['username']
+        password = request.POST['password']
+        user = authenticate(request, username=username, password=password)
+
+
+        if username and password:
+            try:
+                user = get_object_or_404(User, username=username)
+                try:
+                    if user and user.is_active == True:
+                        authenticate_user = authenticate(request, username=username, password=password)
+                        print(authenticate_user)
+                        if authenticate_user:
+                            login(request, authenticate_user)
+                            return redirect('home')
+                        else:
+                            messages.warning(request, "You are not authenticated yet!")
+                            return redirect('login')
+                    else:
+                        messages.warning(request, "Please verify your account to acccess!")
+                        return redirect('login')
+                except:
+                    messages.warning(request, "User not found!")
+                    return redirect('login')
+            except:
+                messages.warning(request, 'Wrong username or email')
+                return redirect('login')
+
+    return render(request, 'login.html')
+
+
+@login_required(login_url='/login')
+def logoutUser(request):
+    logout(request)
+    return redirect('login')
